d573353a563083c3e496cc923fa8231da8125670
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-06-08 11:24:26 +0200

    Changes default jwt key

--- config/index.js
+++ config/index.js
@@ -1,10 +1,9 @@
 'use strict'
-
 const config = {
   SKOLESKYSS_SERVER_PORT_WEB: process.env.SKOLESKYSS_SERVER_PORT_WEB || 8000,
   SKOLESKYSS_DB_CONNECTION_QUEUE: process.env.SKOLESKYSS_DB_CONNECTION_QUEUE || 'mongodb://localhost/skoleskyss',
   SKOLESKYSS_CALLBACK_STATUS_URL: process.env.SKOLESKYSS_CALLBACK_STATUS_URL || 'https://api.buddy.com/status/',
-  SKOLESKYSS_JWT_SECRET: process.env.SKOLESKYSS_JWT_SECRET || 'Louie Louie, oh no, I got to go',
+  SKOLESKYSS_JWT_SECRET: process.env.SKOLESKYSS_JWT_SECRET || 'Louie Louie, oh no, I got to go. Louie Louie, oh no, I got to go',
   SKOLESKYSS_COOKIE_SECRET: process.env.SKOLESKYSS_COOKIE_SECRET || 'Louie Louie, oh no, I got to go. Louie Louie, oh no, I got to go',
   SKOLESKYSS_YAR_SECRET: process.env.SKOLESKYSS_YAR_SECRET || 'Louie Louie, oh no, I got to go. Louie Louie, oh no, I got to go',
   SKOLESKYSS_GITHUB_FEEDBACK_URL: process.env.SKOLESKYSS_GITHUB_FEEDBACK_URL || 'https://api.github.com/repos/:user/:repo/issues',
