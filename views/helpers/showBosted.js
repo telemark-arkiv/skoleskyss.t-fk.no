9d7f5097edd1cb570f3d671b7fd26553ef4cf30a
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-06-20 19:25:07 +0200

    Adds link to map for showBosted

--- views/helpers/showBosted.js
+++ views/helpers/showBosted.js
@@ -10,6 +10,7 @@ module.exports = function showBosted (data) {
     outputs.push(showDsfBosted(data.dsfData))
     if (data['distance-see-dsf']) {
       outputs.push('Foreløpig beregnet avstand til skole: ' + data['distance-see-dsf'].data.distance)
+      outputs.push('<a href="' + data['distance-see-dsf'].data.staticMapUrl + '" target="_blank">Se beregnet rute</a>')
     }
   }
 
@@ -19,12 +20,14 @@ module.exports = function showBosted (data) {
     outputs.push(showDsfBosted(data.dsfData))
     if (data['distance-see-dsf']) {
       outputs.push('Foreløpig beregnet avstand til skole: ' + data['distance-see-dsf'].data.distance)
+      outputs.push('<a href="' + data['distance-see-dsf'].data.staticMapUrl + '" target="_blank">Se beregnet rute</a>')
     }
     outputs.push('')
     outputs.push('Bosted nummer to')
     outputs.push(showDsfBosted(data.bosteddelt))
     if (data['distance-see-delt']) {
       outputs.push('Foreløpig beregnet avstand til skole: ' + data['distance-see-delt'].data.distance)
+      outputs.push('<a href="' + data['distance-see-delt'].data.staticMapUrl + '" target="_blank">Se beregnet rute</a>')
     }
   }
 
@@ -33,6 +36,7 @@ module.exports = function showBosted (data) {
     outputs.push(showDsfBosted(data.bostedhybel))
     if (data['distance-see-hybel']) {
       outputs.push('Foreløpig beregnet avstand til skole: ' + data['distance-see-hybel'].data.distance)
+      outputs.push('<a href="' + data['distance-see-hybel'].data.staticMapUrl + '" target="_blank">Se beregnet rute</a>')
     }
   }
   return outputs.join('<br />')
