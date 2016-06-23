a321788348ecac589ed42d58d180bf8d94021bfa
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-06-23 10:12:55 +0200

    Fixes wrong loginurl for cookie

--- server.js
+++ server.js
@@ -94,7 +94,7 @@ server.register(require('hapi-auth-cookie'), function (err) {
     password: config.SKOLESKYSS_COOKIE_SECRET,
     cookie: 'skoleskyss-session',
     validateFunc: validate,
-    redirectTo: config.SKOLESKYSS_AUTH_URL,
+    redirectTo: config.SKOLESKYSS_AUTH_URL_LOGIN,
     isSecure: false
   })
 
